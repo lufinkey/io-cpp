f877ecf9dd58e69920885cdcac47e5a904fcc8b6
Luis Finke <lfinke@fusionalliance.com>
2021-09-06 13:34:38 -0400

    remove unneeded redefinition of macros

--- src/fgl/io/Common.hpp
+++ src/fgl/io/Common.hpp
@@ -12,27 +12,5 @@
 #include <fgl/async.hpp>
 
 namespace fgl {
-	#ifndef FGL_ASSERT
-		#ifdef NDEBUG
-			#define FGL_ASSERT(condition, message)
-		#else
-			#define FGL_ASSERT(condition, message) { \
-				if(!(condition)) { \
-					std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
-						<< " line " << __LINE__ << ": " << (message) << std::endl; \
-					assert(false); \
-				} \
-			}
-		#endif
-	#endif
-	
-	#ifndef FGL_WARN
-		#ifdef NDEBUG
-			#define FGL_WARN(message)
-		#else
-			#define FGL_WARN(message) { \
-				std::cerr << "Warning: " << (message) << std::endl; \
-			}
-		#endif
-	#endif
+	//
 }
